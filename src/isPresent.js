12aa0443caab57b55a311abb5d19c887cc127544
Roy Stewart <roythomasstewart@gmail.com>
2022-12-16 05:33:43 -0500

    Added a description for isPresent

--- src/isPresent.js
+++ src/isPresent.js
@@ -1,6 +1,6 @@
 /**
- *
- * @param {*} value
+ * checks if a value is present
+ * @param {*} value the value to check
  * @returns {boolean} false if the value is null or undefined, true otherwise
  */
 exports.isPresent = (value) => value !== null && value !== undefined
