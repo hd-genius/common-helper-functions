c4d5c01431b98ef7f4139918c521078a48baf617
Roy Stewart <roythomasstewart@gmail.com>
2022-12-16 06:00:39 -0500

    Fixed unit test

--- src/numbers/isEven.js
+++ src/numbers/isEven.js
@@ -3,4 +3,5 @@
  * @param {number} value the value to test
  * @returns {boolean} true if the value is even, false if the value is not
  */
-exports.isEven = (value) => value !== 0 && value % 2 === 0
+// eslint-disable-next-line eqeqeq
+exports.isEven = (value) => value != 0 && value % 2 === 0
